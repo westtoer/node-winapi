363635d2eeb771991893c4aa6483ba55b9dfcd7f
Marc Portier <MPT@w272.westtoer.be>
2015-07-10 23:25:30 +0200

    updated dump process according to new wishes for website feed
     - abandon grouping and go for flat tourtypes
     - better dir nesting split from name-joining
    using the new bulk feature (no need to calculate size)
    csv output of performed tasks
    play nice and throtle down to 10 requests per second

--- dhubdump.js
+++ dhubdump.js
@@ -15,112 +15,36 @@ var wapi = require('./lib/winapi'),
     outDir,
     secret,
     work = [],
+    time = 0,
+    timeinc,
     FORMATS = {xml: "asXML", json: "asJSON"},
     PERIODS = {week: 7, day: 1},
     PUBSTATES = {all: "ignorePublished", pub: "published" },
+
+    //TODO there should be a dynamic way to retrieve these lists
     PRODUCTS = ['accommodation', 'permanent_offering', 'reca', 'temporary_offering', 'meetingroom'],
-    CHANNELS = ['westtoer', 'brugse_ommeland', 'westhoek', 'de_kust', 'leiestreek', 'fietsen_en_wandelen',
-                'kenniscentrum', 'dagtrips_voor_groepen', 'flanders_fields', 'meetingkust', 'autoroutes',
-                'itrip_coast', 'kustwandelroute', 'west-vlinderen', '300_jaar_grens'],
-    GROUPS = {
-        "gastenkamer": ["gastenkamer"],
-        "hotel": ["hotel"],
-        "kampeerlogies": [
-            "camping",
-            "kampeerautoterrein",
-            "minicamping",
-            "verblijfpark",
-            "kampeerhut"],
-        "logies_voor_doelgroepen": [
-            "jeugdverblijf",
-            "vakantiecentrum"
-        ],
-        "vakantielogies": ["vakantielogies"],
-        "vakantiepark": ["vakantiepark"],
-        "vakantiewoning": ["vakantiewoning"],
-        "andere_attracties": [
-            "attractiepark",
-            "pretpark",
-            "speeltuin"],
-        "fietsverhuur": ["fietsverhuur"],
-        "paardrijden": [
-            "huifkartocht",
-            "manege"
-        ],
-        "sport": [
-            "adventure",
-            "golf",
-            "sportaccommodatie",
-            "zwemgelegenheid"],
-        "toeren": ["scooter_solex_verhuur"],
-        "waterrecreatie_attracties": [
-            "aanlegplaats",
-            "jachthaven",
-            "bootverhuur",
-            "kano_kajak_verhuur",
-            "strandclub"
-        ],
-        "andere": [
-            "bezoekerscentrum",
-            "bioscoop",
-            "casino",
-            "cultureel_centrum",
-            "kinderboerderij",
-            "museum",
-            "shop_winkel",
-            "wellness"
-        ],
-        "architectuur_gebouwen": [
-            "belfort",
-            "historisch_gebouw",
-            "monument",
-            "religieus_gebouw"
-        ],
-        "natuur": [
-            "bos",
-            "domein",
-            "park_tuin"
-        ],
-        "oorlog": [
-            "battle_field_tour",
-            "begraafplaats_amerikaans",
-            "begraafplaats_belgisch",
-            "begraafplaats_commonwealth",
-            "begraafplaats_duits",
-            "begraafplaats_frans",
-            "frontvlucht",
-            "oorlogssite"
-        ],
-        "gastronomie": [
-            "brouwerij",
-            "hoeve_om_te_proeven",
-            "stokerij",
-            "wijngaard"
-        ],
-        "toeristische_dienst": ["toeristische_dienst"],
-        "bistro": ["bistro"],
-        "cafe": ["cafe"],
-        "restaurant": ["restaurant"],
-        "tearoom": ["tearoom"],
-        "concert": ["concert"],
-        "festival": ["festival"],
-        "fietsen": ["fietsen"],
-        "film": ["film"],
-        "herdenkingsplechtigheid": ["herdenkingsplechtigheid"],
-        "ijspiste": ["ijspiste"],
-        "shopping": ["shopping"],
-        "speciale_markt": ["speciale_markt"],
-        "sportwedstrijd": ["sportwedstrijd"],
-        "stoet": ["stoet"],
-        "tentoonstelling": ["tentoonstelling"],
-        "theater": ["theater"],
-        "vuurwerk": ["vuurwerk"],
-        "wandelen": ["wandelen"],
-        "waterrecreatie_events": ["waterrecreatie"],
-        "wekelijkse_markt": ["wekelijkse_markt"],
-        "onbepaald": ["onbepaald"],
-        "mice": ["zaal"]
-    };
+    // TODO when channels are automatic --> try to recognise that lang-variants can be grouped into one!
+    // --> just match for _nl , then add also the variant without _nl
+    CHANNELS = ['westtoer', 'fietsen_en_wandelen', 'kenniscentrum', 'dagtrips_voor_groepen',
+                'flanders_fields', 'flanders_fields_nl', 'flanders_fields_fr', 'flanders_fields_en', 'flanders_fields_de',
+                'leiestreek', 'leiestreek_nl', 'leiestreek_fr', 'leiestreek_en', 'leiestreek_de',
+                'de_kust', 'de_kust_nl', 'de_kust_en', 'de_kust_fr', 'de_kust_de',
+                'brugse_ommeland', 'brugse_ommeland_nl', 'brugse_ommeland_fr', 'brugse_ommeland_de', 'brugse_ommeland_en',
+                'westhoek', 'westhoek_nl', 'westhoek_fr',
+                'meetingkust', 'meetingkust_nl', 'meetingkust_fr',
+                '300_jaar_grens', '300_jaar_grens_nl', '300_jaar_grens_fr', '300_jaar_grens_en', '300_jaar_grens_de',
+                'autoroutes', 'itrip_coast', 'kustwandelroute', 'west-vlinderen', 'iedereen_flandrien'],
+    TOURTYPES = ["aanlegplaats", "adventure", "attractiepark", "battle_field_tour", "begraafplaats_amerikaans", "begraafplaats_belgisch",
+             "begraafplaats_commonwealth", "begraafplaats_duits", "begraafplaats_frans", "belfort", "bezoekerscentrum", "bioscoop",
+             "bistro", "bootverhuur", "bos", "brouwerij", "cafe", "camping", "casino", "concert", "cultureel_centrum", "domein",
+             "festival", "fietsen", "fietsverhuur", "film", "frontvlucht", "gastenkamer", "golf", "herdenkingsplechtigheid",
+             "historisch_gebouw", "hoeve_om_te_proeven", "hotel", "huifkartocht", "ijspiste", "jachthaven", "jeugdverblijf",
+             "kampeerautoterrein", "kampeerhut", "kano_kajak_verhuur", "kinderboerderij", "manege", "minicamping", "monument",
+             "museum", "onbepaald", "oorlogssite", "park_tuin", "pretpark", "religieus_gebouw", "restaurant",
+             "scooter_solex_verhuur", "shopping", "shop_winkel", "speciale_markt", "speeltuin", "sportaccommodatie",
+             "sportwedstrijd", "stoet", "stokerij", "strandclub", "tearoom", "tentoonstelling", "theater", "toeristische_dienst",
+             "vakantiecentrum", "vakantielogies", "vakantiepark", "vakantiewoning", "verblijfpark", "vuurwerk", "wandelen",
+             "waterrecreatie", "wekelijkse_markt", "wellness", "wijngaard", "zaal", "zwemgelegenheid"];
 
 settings = argv
     .usage('Maakt de win2 dump voor de datahub.\nUsage: $0')
@@ -139,51 +63,66 @@ settings = argv
     .alias('v', 'verbose')
     .default('v', false)
 
+    .describe('timebetween', 'wait this many millis between requests')
+    .alias('t', 'timebetween')
+    .default('t', 100)  //10 requests per second
+
     .demand(['secret', 'output'])
 
     .argv;
 
 win = wapi.client({secret: settings.secret, clientid: settings.clientid, verbose: settings.verbose});
 outDir = settings.output;
+timeinc = settings.timebetween;
 
 function addTask(task) {
     work.push(task);
 }
 
+function nameJoin() {
+    return [].reduce.call(arguments, function (name, part) { return (name.length ? name + "-" : "") + part; }, "");
+}
+
+
+function onNextInterval(fn) {
+    setTimeout(fn, time);
+    time += timeinc;
+}
+
 function perform(task) {
-    //console.log(JSON.stringify(task));
+    console.log([
+        task.dir,
+        task.name,
+        task.query.types.join('|'),
+        task.query.touristictypes.join('|'),
+        task.query.channels.join('|'),
+        task.query.lastmodExpr,
+        task.query.softDelState,
+        task.query.pubState
+    ].join(','));
 
-    var q = task.query;
-    // do request 3 times for formats : 1 to retrieve size, then one for each format output
-    win.fetch(q.clone().asJSON_HAL().size(1), function (err, res, meta) {
-        var pathname = path.join(outDir, task.name),
-            SIZEMARGIN = 100,
-            size = meta.pages;
+    var q = task.query,
+        pathname = path.join(outDir, task.dir, task.name);
 
-        function remove(ext) {
-            var fname = [pathname, ext].join('.');
-            if (fs.existsSync(fname)) {
-                fs.unlinkSync(fname);
-            }
+    function remove(ext) {
+        var fname = [pathname, ext].join('.');
+        if (fs.existsSync(fname)) {
+            fs.unlinkSync(fname);
         }
+    }
 
-        if (size === 0) {
-            console.log("no data for %s - removing files", pathname);
-            Object.keys(FORMATS).forEach(remove);
-        } else {
-            size += SIZEMARGIN;
-            Object.keys(FORMATS).forEach(function (ext) {
-                var fmtMethod = FORMATS[ext],
-                    sink = fs.createWriteStream([pathname, ext].join('.'));
+    Object.keys(FORMATS).forEach(function (ext) {
+        var fmtMethod = FORMATS[ext],
+            sink = fs.createWriteStream([pathname, ext].join('.'));
 
-                sink.on('error', function (e) {
-                    console.error("error saving " + pathname + "." + ext + "-->" + e);
-                    remove(ext);
-                });
+        sink.on('error', function (e) {
+            console.error("error saving " + pathname + "." + ext + "-->" + e);
+            remove(ext);
+        });
 
-                win.stream(q.clone().size(size)[fmtMethod](), sink);
-            });
-        }
+        onNextInterval(function () {
+            win.stream(q.clone().bulk()[fmtMethod](), sink);
+        });
     });
 }
 
@@ -192,7 +131,8 @@ function makePeriodTasks(pTask) {
     return function (period) {
         var days = PERIODS[period],
             from = to.clone().subtract(days, 'days'),
-            task = {name: [pTask.name, period, from.format('YYYYMMDD'), to.format('YYYYMMDD')].join('-'),
+            task = {dir  : pTask.dir,
+                    name : nameJoin(pTask.name, period, from.format('YYYYMMDD'), to.format('YYYYMMDD')),
                     query: pTask.query.clone().lastmodBetween(from, to)};
         addTask(task);
     };
@@ -201,7 +141,8 @@ function makePeriodTasks(pTask) {
 function makePubTasks(pTask) {
     return function (pubKey) {
         var pubMethod = PUBSTATES[pubKey],
-            task = {name: [pTask.name, pubKey].join('-'),
+            task = {dir  : pTask.dir,
+                    name : nameJoin(pTask.name, pubKey),
                     query: pTask.query.clone()[pubMethod]()};
         addTask(task);
         Object.keys(PERIODS).forEach(makePeriodTasks(task));
@@ -214,30 +155,52 @@ function assertDirExists(dir) {
     }
 }
 
-function makeChannelTasks(pTask) {
-    assertDirExists(path.join(outDir, "bychannel"));
-    return function (channel) {
-        var name = [pTask.name, channel, "pub"].join('-'),
-            task = {name: path.join("bychannel", channel, name),
-                    query: pTask.query.clone().published().forChannels(channel + "*")};
+function makeTourTypeTasks(pTask) {
+    assertDirExists(path.join(outDir, pTask.dir, "bytourtype"));
+    return function (tourtype) {
+        var dir  = path.join(pTask.dir, "bytourtype", tourtype),
+            task = {dir  : dir,
+                    name : nameJoin(pTask.name, tourtype),
+                    query: pTask.query.clone().forTouristicTypes(tourtype)};
 
-        assertDirExists(path.join(outDir, "bychannel", channel));
+        assertDirExists(path.join(outDir, dir));
 
         addTask(task);
+        Object.keys(PERIODS).forEach(makePeriodTasks(task));
+    };
+}
+
+function makeChannelTasks(pTask) {
+    assertDirExists(path.join(outDir, pTask.dir, "bychannel"));
+    return function (channel) {
+        var dir  = path.join(pTask.dir, "bychannel", channel),
+            task = {dir  : dir,
+                    name : nameJoin(pTask.name, channel, "pub-all"),
+                    query: pTask.query.clone().published().forTypes(PRODUCTS).forChannels(channel + "*")};
+
+        assertDirExists(path.join(outDir, dir));
+
+        addTask(task);
+        Object.keys(PERIODS).forEach(makePeriodTasks(task));
+
+        //simplify name downwards -
+        task.name = nameJoin(pTask.name, channel, "pub");
+        TOURTYPES.forEach(makeTourTypeTasks(task));
     };
 }
 
 function makeProductTasks(pTask) {
     return function (prod) {
-        var task = {name: prod,
+        var task = {dir  : pTask.dir,
+                    name : prod,
                     query: pTask.query.clone().forTypes(prod)};
         Object.keys(PUBSTATES).forEach(makePubTasks(task));
-        CHANNELS.forEach(makeChannelTasks(task));
     };
 }
 function makeAll() {
-    var task = { name: "", query: wapi.query('product')};
+    var task = {dir: ".", name: "", query: wapi.query('product')};
     PRODUCTS.forEach(makeProductTasks(task));
+    CHANNELS.forEach(makeChannelTasks(task));
 }
 
 function makeDump() {
