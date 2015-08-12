4b198ff235bad83af31dea2b0398be228730f88f
Marc Portier <marc.portier@gmail.com>
2015-08-12 14:42:39 +0200

    add extra test to verify parallel clients with the same settings

--- test/test-authtoken.js
+++ test/test-authtoken.js
@@ -10,12 +10,12 @@ var chai = require('chai'),
     expect = chai.expect,
     path = require('path'),
     settings = require('./client-settings.json'),
-    wapi = require('../lib/winapi'),
-    win = wapi.client(settings);
+    wapi = require('../lib/winapi');
 
 
 describe('authentication testing', function () {
     it('should have a valid token after start', function (done) {
+        var win = wapi.client(settings);
         assert.isNull(win.token, "token should be undefined before start.");
         win.start(function () {
             assert.isDefined(win.token, "token should not be undefined after start.");
@@ -25,4 +25,35 @@ describe('authentication testing', function () {
             done();
         });
     });
+
+    it('should be able to operate two clients in parallel', function (done) {
+        var win = wapi.client(settings),
+            win2 = wapi.client(settings),
+            q = wapi.query('product').size(1).asJSON_HAL();
+
+        assert.isNull(win.token, "token should be undefined before start.");
+        assert.isNull(win2.token, "token2 should be undefined before start.");
+
+        win.start(function () {
+            assert.isNotNull(win.token, "token should not be null after start.");
+            assert(win.token.length > 0, "token should be not zero-length string after start.");
+            win2.start(function () {
+                assert.isNotNull(win2.token, "token2 should not be null after start.");
+                assert(win2.token.length > 0, "token2 should be not zero-length string after start.");
+                assert(win.token !== win2.token, "independent clients should have different tokens");
+                win.fetch(q.clone(), function (e, o, m) {
+                    assert.isNull(e, "there should be no error in nested fetch");
+                    assert.equal(o.length, 1, "data retrieval should work");
+                    win2.fetch(q.clone(), function (e2, o2, m2) {
+                        assert.isNull(e2, "there should be no error in nested fetch");
+                        assert.equal(o2.length, 1, "nested data retrieval should work");
+                        win.stop();
+                        win2.stop();
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
 });
