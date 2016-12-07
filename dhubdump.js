22b4fc33d9052927c69193dd6c45cbf4bbe87379
Marc Portier <marc.portier@gmail.com>
2016-12-07 15:57:02 +0100

    add filter export for brugge

--- dhubdump.js
+++ dhubdump.js
@@ -50,7 +50,7 @@ var wapi = require('./lib/winapi'),
              "sportwedstrijd", "stoet", "stokerij", "strandclub", "tearoom", "tentoonstelling", "theater", "toeristische_dienst",
              "vakantiecentrum", "vakantielogies", "vakantiepark", "vakantiewoning", "verblijfpark", "vuurwerk", "wandelen",
              "waterrecreatie", "wekelijkse_markt", "wellness", "wijngaard", "zaal", "zwemgelegenheid"],
-    CITIES = ["Koksijde", "Oostende", "Poperinge", "Heuvelland"];
+    CITIES = ["Koksijde", "Oostende", "Poperinge", "Heuvelland", "Brugge"];
 
 settings = argv
     .usage('Maakt een dump op basis van de win2 API.\nUsage: $0')
